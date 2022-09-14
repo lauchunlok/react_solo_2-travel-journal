cc383093ee402f04dadd48cf7b20f38170f890a6
Chun Lok Lau <lauchunlok1@gmail.com>
2022-09-14 16:14:26 +0100

    update image path to local file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from "react"
-import NavBar from "./components/NavBar"
-import Card from "./components/Card"
-import data from "./data"
+import React from "react";
+import NavBar from "./components/NavBar";
+import Card from "./components/Card";
+import data from "./data";
 
 export default function App() {
-    const cards = data.map(item => {
-        return (
-            <Card
-            key={item.id}
-			item={item}
-			                
-			/>
-		)
-	})    
-    
-    
-    return (
-        <div>
-            <NavBar />
-            {cards}
+  const cards = data.map((item) => {
+    return <Card key={item.id} item={item} />;
+  });
 
-        </div>
-    )
+  return (
+    <div>
+      <NavBar />
+      {cards}
+    </div>
+  );
 }
